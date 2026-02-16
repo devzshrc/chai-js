d837d7fe3c64aa4c1562c66aeb5d393c52469464
devzshrc <de5ash1zh@gmail.com>
2026-02-16 22:59:54 +0530

    day02

--- 03-datatypes.js
+++ 03-datatypes.js
@@ -19,3 +19,40 @@ let weatherApiResponse = null; // null is better than 0, cuz 0 is a value
 //null can be handled
 console.log(weatherApiResponse);
 //quirk: typeof null is null in JS - a bug due to backward compatibility
+
+const uniqueRuneId = Symbol("rune_of_fire");
+console.log("Rune: ", uniqueRuneId.toString(), "\ typeof: ", typeof uniqueRuneId);
+//use .toString() when working with symbol as a precaution
+
+//how many primitives?
+
+//non-primitives - objects....
+const heroStats = {
+  name: "Deepak",
+  level: 42,
+  class: "Ranger",
+};
+console.log("Hero: ", heroStats, "| type: ", typeof heroStats);
+
+const inventory = ["Flame sword", "Health potion", "Sheild"];
+console.log("Inventory: ", inventory, "| type: ", typeof inventory);
+
+function castSpell() {
+  return "Fireball";
+}
+console.log("Spell Type: ", typeof castSpell);
+// even though it says function type as function, but internally it is an object
+
+console.log(typeof "chaicode");
+console.log(typeof 42);
+console.log(typeof 42n);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof Symbol());
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function(){});
+
+
+
